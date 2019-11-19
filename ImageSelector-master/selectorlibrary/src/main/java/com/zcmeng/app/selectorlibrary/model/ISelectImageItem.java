901b6e0513583a8f1aa9366f1fe08bd87fd84d48
zcmeng <1042086562@qq.com>
2019-11-19 18:24:09 +0800

    提交代码

---
+++ ImageSelector-master/selectorlibrary/src/main/java/com/zcmeng/app/selectorlibrary/model/ISelectImageItem.java
@@ -0,0 +1,11 @@
+package com.zcmeng.app.selectorlibrary.model;
+
+/**
+ * Created by yixuanxuan on 16/10/12.
+ */
+
+public interface ISelectImageItem {
+    String getImgPath();
+    boolean isChecked();
+    void setChecked(boolean checked);
+}
